f29726ee3054ca57b05a52eebf67f0a95f031685
Elijah Itah <eitah@cars.com>
2017-12-02 15:38:14 -0600

    Non prod code

--- src/flux/helpers.js
+++ src/flux/helpers.js
@@ -1,12 +1,7 @@
 import {fluxConfig} from './flux_config.js';
 
-// instantiate the Flux SDK with your appliation key
-export const sdk = new FluxSdk(fluxConfig.flux_client_id, {
-  redirectUri: fluxConfig.url,
-  fluxUrl: fluxConfig.flux_url
-});
 
-export const helpers = new FluxHelpers(sdk);
+
 
 // export const sdk = () => {}
-// export const helpers = () => {}
+export const helpers = () => {}
