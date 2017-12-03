703fe005487814fb78162ceb51b87b780e76fc4d
hsinkoff <hsinkoff@hotmail.com>
2017-12-03 08:37:00 -0600

    remove input, add exif

--- src/GoogleVision/GoogleVision.js
+++ src/GoogleVision/GoogleVision.js
@@ -4,12 +4,27 @@
  */
 import React from 'react';
 
+/* @flow */
+let ExifImage = require('exif').ExifImage;
+
+// import gapi from 'google-client-api';
+
+// import GoogleVisionClient from '@google-cloud/vision';
+
 class GoogleVision extends React.Component {
   componentWillUpdate = (nextProps) => {
     if (nextProps && nextProps.picture) {
       const detail = { picture: nextProps.picture };
       const readyEvent = new CustomEvent('analyzePicture', { detail });
       document.dispatchEvent(readyEvent);
+      new ExifImage({ image :  nextProps.picture}, function (error, exifData) {
+        if (error)
+          console.log('Error: '+error.message);
+        else
+          console.log(exifData); // Do something with your data!
+      });
+      debugger;
+
     }
   };
 
